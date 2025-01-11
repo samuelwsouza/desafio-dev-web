270aa1ad2e8caeba0efaee6465acafb759ad0334
Samuel Souza <samukawsouza@gmail.com>
2025-01-11 00:20:44 -0300

    ui design main done

--- index.php
+++ index.php
@@ -1,14 +1,55 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema de Cadastro</title>
-    <link rel="stylesheet" href="css/style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sistema de Cadastro</title>
+  <link rel="icon" type="image/x-icon" href="/assets/favicon.ico">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
+
 <body>
-   <header>
-    
-   </header>
+  <header id="header">
+    <div class="container">
+      <img src="./assets/logoHeader.png" alt="">
+      <nav class="nav-centro">
+        <ul>
+          <li><a href="">All Preview</a></li>
+          <li><a href="">Feature</a></li>
+          <li><a href="">Docs</a></li>
+          <li><a href="">Need Help?</a></li>
+        </ul>
+
+        <div class="btn-center">
+          <button class="button-header">Desafio</button>
+        </div>
+      </nav>
+
+    </div>
+  </header>
+
+  <main>
+    <h5>Introduzindo</h5>
+    <h1>Desafio Web Júnior</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias iste maiores voluptatum mollitia dolorum ipsam excepturi nisi? Laborum aliquam, vel quasi tenetur voluptatibus quod consequuntur quam nobis dolore, ducimus ea!</p>
+    <div class="main-buttons">
+      <button class="table-btn">Ir para a Table</button>
+      <button class="linkedin-btn">LinkedIn</button>
+    </div>
+    <div class="techs">
+      <img src="./assets/html5-icon.svg" alt="">
+      <img src="./assets/css3-icon.svg" alt="">
+      <img src="./assets/js-icon.svg" alt="">
+      <img src="./assets/php-icon.svg" alt="">
+    </div>
+  </main>
+
+  <script src="js/header.js"></script>
+
 </body>
+
 </html>

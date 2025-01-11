a7b323847d5cb67746a5bb26d7e76db139af13d7
Samuel Souza <samukawsouza@gmail.com>
2025-01-11 00:20:10 -0300

    scrolled fixed header implemented

---
+++ js/header.js
@@ -0,0 +1,9 @@
+document.addEventListener("scroll", () => {
+    const header = document.getElementById("header");
+
+    if (window.scrollY > 0) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
